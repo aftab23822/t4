fc8effb9322578e5bdbb1666c1fef98c4fed9531
aftab23822 <58620536+aftab23822@users.noreply.github.com>
2020-08-17 20:52:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,65 +1,152 @@
+#-*- coding: utf-8 -*-
+
+import json
+import codecs
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+import re
+import subprocess
+from telegram.ext.dispatcher import run_async
+from telegram.ext import Updater
+from html import escape
+
+updater = Updater(token='1382362482:AAHd79jriWS56eXQZRwFP1NoqeB6K3pE_qg')
+dispatcher = updater.dispatcher
+
 import logging
-import os
-PORT = int(os.environ.get('PORT', 5000))
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
-PORT = int(os.environ.get('PORT', 5000))
-
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+					level=logging.INFO)
 
-logger = logging.getLogger(__name__)
-TOKEN = '1382362482:AAHd79jriWS56eXQZRwFP1NoqeB6K3pE_qg'
+def commands(bot, update):
+	user = update.message.from_user.username
+	bot.send_message(chat_id=update.message.chat_id, text="Initiating commands /tip & /withdraw have a specfic format,\n use them like so:" + "\n \n Parameters: \n <user> = target user to tip \n <amount> = amount of idealCash to utilise \n <address> = idealCash address to withdraw to \n \n Tipping format: \n /tip <user> <amount> \n \n Withdrawing format: \n /withdraw <address> <amount>")
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+def help(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text="The following commands are at your disposal: /hi , /commands , /deposit , /tip , /withdraw , or /balance")
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+def deposit(bot, update):
+	user = update.message.from_user.username
+	if user is None:
+		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	else:
+		address = "/usr/bin/idealcashd"
+		result = subprocess.run([address,"getaccountaddress",user],stdout=subprocess.PIPE)
+		clean = (result.stdout.strip()).decode("utf-8")
+		bot.send_message(chat_id=update.message.chat_id, text="@{0} your depositing address is: {1}".format(user,clean))
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def tip(bot,update):
+	user = update.message.from_user.username
+	target = update.message.text[5:]
+	amount =  target.split(" ")[1]
+	target =  target.split(" ")[0]
+	if user is None:
+		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	else:
+		machine = "@IdealCash_Tipbot"
+		if target == machine:
+			bot.send_message(chat_id=update.message.chat_id, text="HODL.")
+		elif "@" in target:
+			target = target[1:]
+			user = update.message.from_user.username
+			core = "/usr/bin/idealcashd"
+			result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
+			balance = float((result.stdout.strip()).decode("utf-8"))
+			amount = float(amount)
+			if balance < amount:
+				bot.send_message(chat_id=update.message.chat_id, text="@{0} you have insufficent funds.".format(user))
+			elif target == user:
+				bot.send_message(chat_id=update.message.chat_id, text="You can't tip yourself!")
+			else:
+				balance = str(balance)
+				amount = str(amount)
+				tx = subprocess.run([core,"move",user,target,amount],stdout=subprocess.PIPE)
+				bot.send_message(chat_id=update.message.chat_id, text="@{0} tipped @{1} of {2} DEAL".format(user, target, amount))
+		else:
+			bot.send_message(chat_id=update.message.chat_id, text="Error that user is not applicable.")
 
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+def balance(bot,update):
+	user = update.message.from_user.username
+	if user is None:
+		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	else:
+		core = "/usr/bin/idealcashd"
+		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
+		clean = (result.stdout.strip()).decode("utf-8")
+		balance  = float(clean)
+		balance =  str(round(balance,3))
+		bot.send_message(chat_id=update.message.chat_id, text="@{0} your current balance is: {1} DEAL ".format(user,balance))
 
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+def withdraw(bot,update):
+	user = update.message.from_user.username
+	if user is None:
+		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	else:
+		target = update.message.text[9:]
+		address = target[:35]
+		address = ''.join(str(e) for e in address)
+		target = target.replace(target[:35], '')
+		amount = float(target)
+		core = "/usr/bin/idealcashd"
+		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
+		clean = (result.stdout.strip()).decode("utf-8")
+		balance = float(clean)
+		if balance < amount:
+			bot.send_message(chat_id=update.message.chat_id, text="@{0} you have insufficent funds.".format(user))
+		else:
+			amount = str(amount)
+			tx = subprocess.run([core,"sendfrom",user,address,amount],stdout=subprocess.PIPE)
+			bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrew to address: {1} of {2} DEAL" .format(user,address,amount))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+def hi(bot,update):
+	user = update.message.from_user.username
+	bot.send_message(chat_id=update.message.chat_id, text="Hello @{0}, Let's make a DEAL!".format(user))
 
-    # log all errors
-    dp.add_error_handler(error)
+def rain(bot,update):
+  bot.send_message(chat_id=update.message.chat_id, text="Stake for a rainy day!")
 
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://t444.herokuapp.com/' + TOKEN)
+def price(bot,update):
+	data = requests.get('https://icqbase.com/page/api?method=singleinfo&marketid=69').json()['return'][0]
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+	satsPrice = data['lasttradeprice']
+	change = data['change']
+	volume24 = data['24BaseVolume']
 
-if __name__ == '__main__':
-    main()
+	priceInfo = '''\
+	Price: {0} BTC
+	Change: {1}%
+	24/Total Vol.: {2} BTC'''.format(satsPrice, change, volume24)
+
+	bot.send_message(chat_id=update.message.chat_id, text=priceInfo)
+
+from telegram.ext import CommandHandler
+
+commands_handler = CommandHandler('commands', commands)
+dispatcher.add_handler(commands_handler)
+
+rain_handler = CommandHandler('rain', rain)
+dispatcher.add_handler(rain_handler)
+
+hi_handler = CommandHandler('hi', hi)
+dispatcher.add_handler(hi_handler)
+
+withdraw_handler = CommandHandler('withdraw', withdraw)
+dispatcher.add_handler(withdraw_handler)
+
+deposit_handler = CommandHandler('deposit', deposit)
+dispatcher.add_handler(deposit_handler)
+
+tip_handler = CommandHandler('tip', tip)
+dispatcher.add_handler(tip_handler)
+
+balance_handler = CommandHandler('balance', balance)
+dispatcher.add_handler(balance_handler)
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
+
+price_handler = CommandHandler('price', price)
+dispatcher.add_handler(price_handler)
+
+updater.start_polling()
